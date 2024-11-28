9060ae4b01f24d23e672fad00c72b4a1acf6410d
Aditya Ranjan Das <105553434+Aditya6371@users.noreply.github.com>
2024-11-28 21:48:13 +0530

    rsa encrypt/decrypt

--- demo/home_page.dart
+++ demo/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:getxdemo/custom_textfield.dart';
+import 'package:fast_rsa/fast_rsa.dart';
+
+//flutter pub add fast_rsa
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -17,7 +20,31 @@ class _MyHomePageState extends State<MyHomePage> {
 
   final _key = GlobalKey<FormState>();
 
-  bool _searchEnable = true;
+  bool _searchEnable = false;
+  String message = 'Test for encrypt/decrypt RSA';
+
+  var privateKey = '''-----BEGIN RSA PRIVATE KEY-----
+MIICXQIBAAKBgQCmIyo8x9PuWa7GasppkFlhwcTEo4w5L5OPlBm00HG+rovf9cCN
+S35HBrmK0KSWximOFEzvqaDyQc/G9HEZse7IxOLCOHv5w2aNdkZ/kW/BolIqAiWF
+7cU9HbcNqr2UqT3L40no/IKUoMxSmdUu0Li9YD31XOpj25d8KmbC/HR/XwIDAQAB
+AoGAPX6SqcDFf1oOyT7KAVz+2KJidO6cfqZPljuZcEYfdBcjixhp12L0MNIaZ3ln
+Eq/yvbZh6+v7uPAHOntMSdiEq41+YwV2lLPCZNyOTEyAxIyQzH95UZmjF+/egpZD
+dqg2+AAZt7btTpuQZI1X7RbfThlEPTkEt69M4n9qWWotb8ECQQDuXh9YgJemskha
+ZxxDyyp1w/0rcJQV+nEPGi2En3dt6SRAc3mdCwvP3G8bOv6oiMWlB8WVXGyuTe9y
+RlKHLXGRAkEAsm0+ryq+jgNZZztBVabxftsz0nH71lmAkf7jmikJoJUMXadZObfE
+KISqrcUoAGBgKPDDLHGnhWErf/QbZMNp7wJBAJZtwdxpcssZcf3TWowSGB6v7ALR
+DjN8lIMSYy1yMb2kR2OBPHt2MCMimt+VcIbNoeWPLQsgg9nQh08XwBdc/3ECQGLR
+Lg5FVhPAtxr9LkoJk2X2cNT0W81y9EnnKJaQc8yDv71+mWPl6rWmBAEJWAYdWiRS
+c0WCDI1KK5VJ1IIR1/ECQQCeVTiNS77HTDY8ytnBff7/tP8IOyQV/t3Vo7H2Yyte
+0p/rYuahDzIbNsLplnrFFv/DsyMEOalJJAh857dBliFp
+-----END RSA PRIVATE KEY-----''';
+
+  var publicKey = '''-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCmIyo8x9PuWa7GasppkFlhwcTE
+o4w5L5OPlBm00HG+rovf9cCNS35HBrmK0KSWximOFEzvqaDyQc/G9HEZse7IxOLC
+OHv5w2aNdkZ/kW/BolIqAiWF7cU9HbcNqr2UqT3L40no/IKUoMxSmdUu0Li9YD31
+XOpj25d8KmbC/HR/XwIDAQAB
+-----END PUBLIC KEY-----''';
 
   @override
   Widget build(BuildContext context) {
@@ -90,57 +117,63 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Form(
         key: _key,
-        child: Center(
-          child: Container(
-            padding: const EdgeInsets.all(22),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CustomTextField(
-                  controller: emailcontroller,
-                  labelText: 'Email',
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Email field cannot be empty';
-                    }
-                    const emailPattern =
-                        r'^[a-zA-Z0-9.a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$';
-                    final regex = RegExp(emailPattern);
-                    if (!regex.hasMatch(value)) {
-                      return 'Enter a valid email address';
-                    }
-                    return null;
-                  },
+        child: Container(
+          padding: const EdgeInsets.all(22),
+          child: Column(
+            children: [
+              CustomTextField(
+                controller: emailcontroller,
+                labelText: 'Enter Text to be encrypt',
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Email field cannot be empty';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              CustomButton(
+                title: const Text(
+                  "ENCRYPT",
+                  style: TextStyle(color: Colors.white),
                 ),
-                const SizedBox(
-                  height: 16,
+                onPressed: () async {
+                  if (_key.currentState!.validate()) {
+                    var result = await RSA.encryptPKCS1v15(
+                        emailcontroller.text, publicKey);
+                    message = result;
+                    print(message);
+                    setState(() {});
+                  }
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              CustomButton(
+                title: const Text(
+                  "DECRYPT",
+                  style: TextStyle(color: Colors.white),
                 ),
-                CustomTextField(
-                  controller: passcontroller,
-                  labelText: 'Password',
-                  isPassword: true,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Password field cannot be empty';
-                    }
-                    if (value.length < 8) {
-                      return 'Password must be at least 8 characters long';
-                    }
-                    const passwordPattern =
-                        r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$';
-                    final regex = RegExp(passwordPattern);
-                    if (!regex.hasMatch(value)) {
-                      return 'Password must include uppercase, lowercase, number, and special character';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                // const CustomButton()
-              ],
-            ),
+                onPressed: () async {
+                  if (_key.currentState!.validate()) {
+                    var result = await RSA.decryptPKCS1v15(message, privateKey);
+                    message = result;
+                    print(message);
+                    setState(() {});
+                  }
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(message),
+            ],
           ),
         ),
       ),
